6616164fbb0aa1f54e727f2d94d7e2c9995e6837
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-04-23 21:43:36 +0200

    +JWT Model

---
+++ ZaklepToClientLibrary/Models/Jwt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZaklepToClientLibrary.Models
+{
+    public class Jwt
+    {
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
