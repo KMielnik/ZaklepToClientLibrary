d944e815dc34ff362dfb7680f0594db96946a8ef
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-04-23 20:12:34 +0200

    +Exceptions

---
+++ ZaklepToClientLibrary/Exceptions/ClientException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ZaklepToClientLibrary.Exceptions
+{
+    public class ClientException : Exception
+    {
+        public string Code { get; }
+
+        public ClientException()
+        {
+            
+        }
+
+        public ClientException(string code)
+        {
+            Code = code;
+        }
+
+        public ClientException(string message, params object[] args) : this(string.Empty, message, args)
+        {
+            
+        }
+
+        public ClientException(string code, string message, params object[] args) : this(null, string.Empty, message, args)
+        {
+            
+        }
+
+        public ClientException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+            
+        }
+
+        public ClientException(Exception innerException, string code, string message, params object[] args)
+            : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ ZaklepToClientLibrary/Exceptions/ErrorCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace ZaklepToClientLibrary.Exceptions
+{
+    public class ErrorCodes
+    {
+        public static string InvalidLoginCredentials => "invalid_login_credentials";
+        public static string ResourceAlreadyExists => "resource_already_exists";
+        public static string UserNotFound => "user_not_fond";
+        public static string UserNotLoggedIn => "user_not_logged_in";
+    }
+}
