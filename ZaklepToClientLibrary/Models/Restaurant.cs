7a125f147e3ed1c8c3119423199b46fe1e0322b6
MaciejKaca <maciek.kaca@gmail.com>
2018-04-26 13:14:33 +0200

    +Restaurant model

--- ZaklepToClientLibrary/Models/Restaurant.cs
+++ ZaklepToClientLibrary/Models/Restaurant.cs
@@ -11,5 +11,19 @@ namespace ZaklepToClientLibrary.Models
         public string Cuisine { get; set; }
         public string Localization { get; set; }
         public IEnumerable<Table> Tables { get; set; }
+
+        public Restaurant()
+        {
+        }
+
+        public Restaurant(Guid id, string name, string description, string cuisine, string localization, IEnumerable<Table> tables)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Cuisine = cuisine;
+            Localization = localization;
+            Tables = tables;
+        }
     }
 }
