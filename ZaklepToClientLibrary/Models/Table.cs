f45aff970ecb570a02a226fa694f306763256cce
MaciejKaca <maciek.kaca@gmail.com>
2018-04-26 13:12:09 +0200

    +Table model

--- ZaklepToClientLibrary/Models/Table.cs
+++ ZaklepToClientLibrary/Models/Table.cs
@@ -9,5 +9,16 @@ namespace ZaklepToClientLibrary.Models
         public Guid Id { get; set; }
         public int NumberOfSeats { get; set; }
         public (int x, int y) Coordinates { get; set; }
+
+        public Table()
+        {
+        }
+
+        public Table(Guid id, int numberOfSeats, (int x, int y) coordinates)
+        {
+            Id = id;
+            NumberOfSeats = numberOfSeats;
+            Coordinates = coordinates;
+        }
     }
 }
